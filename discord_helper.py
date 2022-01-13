c56a41b9b8c523583598db57af96b84c792a3b48
vovebry <32960833+vovebry@users.noreply.github.com>
2022-01-13 17:07:01 -0500

    Update discord_helper.py

--- discord_helper.py
+++ discord_helper.py
@@ -105,7 +105,7 @@ class discordWebhook:
         """
         Send the JSON formated object to the specified `self.url`.
         """
-        if url is not None and url != 'https://discord.com/api/webhooks/931166337894608966/FKjTHP_J40vov008ZwsEeV6D2OtKO4zOCWDAeJ7Kz9NYxHQFZC6KKVFybwKxdO0oKZAI':
+        if url is not None and url != '':
             headers = {'Content-Type': 'application/json'}
             result = requests.post(url, data=self.json, headers=headers)
 
